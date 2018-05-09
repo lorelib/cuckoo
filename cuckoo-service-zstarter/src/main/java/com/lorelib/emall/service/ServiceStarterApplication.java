a73da338426b3d5e8b532cc8a9d2125bc9829b7c
listening <893979653@qq.com>
2018-05-09 21:23:00 +0800

    服务启动模块

---
+++ cuckoo-service-zstarter/src/main/java/com/lorelib/emall/service/ServiceStarterApplication.java
@@ -0,0 +1,17 @@
+package com.lorelib.cuckoo.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author listening
+ * @description
+ * @date 2018-05-08 10:49
+ * @since 1.0
+ */
+@SpringBootApplication
+public class ServiceStarterApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceStarterApplication.class, args);
+  }
+}
